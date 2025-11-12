cd6d9b5e0b7241896496d61a040c7887e70f708f
Ariannab1308 <ariannab1308@gmail.com>
2025-11-12 20:41:24 +0000

    Week13 no.4

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -22,31 +22,28 @@ print(1 < x < 20)         # True
 score = int(input("Enter your score (0-100): "))
 # if score is between 90 and 100
 # assign grade A
-if 90 <= score <= 100:
+if score >= 90 and score <= 100:
     print("Grade: A")
-# if score is between 80 and 89
-# assign grade B
-elif 80 <= score < 90:
+elif score >= 80 and score < 90:
     print("Grade: B")
-# if score is between 70 and 79 
-# assign grade C
-elif 70 <= score < 80:
+elif score >= 70 and score < 80:
     print("Grade: C")
-# if score is between 60 and 69
-# assign grade D
-elif 60 <= score < 70:
+if score >= 60 and score < 70:
     print("Grade: D")
-# if score is below 60 
-# assign grade f
-else:
-    print("Grade: F")
+elif score < 60:
+    print("Grade: F")  
 # Practice Problems:
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
+number = int(input("Enter a number:"))
+if number >= 50 and number <= 100:
+    print("Number is valid.")
+else:
+    print("Number is invalid.")
 
 # Write an expression that checks if a number is NOT equal to 0 and greater than 10.
-
+x = int(input("Enter another number:"))
+print(x != 0 and x > 10)
 # Use chained comparison to check if 3 < 4 < 5.
 
 # Challenge: Create a password rule using logical operators:
-
